2e22b882fa7a90d65ae9699e7cb1d34f8a9ccce2
Victor persson <vicpe517@student.liu.se>
2023-03-09 11:20:33 +0100

    Kastfiende
    Implementerat en fiende som tar in en target.
    
    Airtimen för det instantierade objektet kan skräddarsys så att vi kan bestämma exakt hur lång tid det tar för objektet att nå dess target (dvs vår kamera).
    Längre tid = Högre kastvinkel och mer kraft
    
    Om target är långt ifrån kastar den hårdare för att kompensera
    Om target är nära kastar den lösare och högre
    
    Fienden träffar alltid sin target efter exakt den tid man beskriver om target inte rör på sig
    
    Kommer även med 2 goofy ljudeffekter

---
+++ Assets/enemyThrower.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyThrower : MonoBehaviour
+{
+    public GameObject target;
+    public Rigidbody projectilePrefab;
+
+    public AudioSource audioSourceThrow;
+    public AudioSource audioSourceHit;
+    public AudioClip clip;
+    public AudioClip clipHit;
+    public float throwVolume = 0.3f;
+    public float hitVolume = 0.6f;
+
+    public float timeToTarget = 5.0f;
+    //public float throwFrequency = 4.0f;
+    public float currTime = 0.0f;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+        currTime += Time.deltaTime;
+
+        if(timeToTarget == 0.0f) timeToTarget = 0.01f;
+        
+        transform.LookAt(target.transform);
+
+        transform.position = new Vector3(Mathf.Sin(Time.time)*10.0f,-1.8f,Mathf.Cos(Time.time)*10.0f);
+
+        if(currTime > timeToTarget){
+            audioSourceThrow.PlayOneShot(clip, throwVolume);
+
+            audioSourceHit.PlayDelayed(timeToTarget);
+
+            float distance = (target.transform.position - gameObject.transform.position).magnitude;
+            float gravitys = Physics.gravity.magnitude;
+
+            float V_0x = distance/timeToTarget;
+            float V_0y = (gravitys*(timeToTarget)*(timeToTarget)/2.0f)/timeToTarget;
+            
+            currTime = 0.0f;
+            Rigidbody projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+            // Calculate the direction to the target
+            Vector3 targetDirection = target.transform.position - transform.position;
+            targetDirection.y = 0f;
+
+            // Calculate the angle to the target
+            float angle = Vector3.Angle(targetDirection, transform.forward);
+
+            // Rotate the projectile towards the target
+            projectile.transform.rotation = Quaternion.LookRotation(targetDirection);
+
+            // Calculate the initial velocity of the projectile
+            Vector3 initialVelocity = projectile.transform.forward * V_0x;
+            initialVelocity.y = V_0y;
+
+            // Set the velocity of the projectile
+            projectile.velocity = initialVelocity;
+        }
+    }
+}
+
+

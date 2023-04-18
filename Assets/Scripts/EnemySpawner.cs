ab37ff3031abf0f978f391d515bcf85310d613cf
Albin Kjellberg <albin.kjellberg2000@gmail.com>
2023-04-19 01:12:58 +0200

    Implemented new enemy spawner functionality (random in front of player)

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -5,58 +5,64 @@ using UnityEngine;
 // https://subscription.packtpub.com/book/game-development/9781838828424/2/ch02lvl1sec20/setting-up-our-enemyspawner-and-enemy-scripts
 public class EnemySpawner : MonoBehaviour // spawns enemy with "spawnInterval" seconds inbetween
 {
-    public GameObject enemy;            // Enemy prefab.
-    //private GameObject[] enemies;     // For later use with multiple types of enemies.
-    public GameObject playerGameObject; // Player gameobject
+  public GameObject enemy;            // Enemy prefab.
+                                      //private GameObject[] enemies;     // For later use with multiple types of enemies.
+  public GameObject player; // Player gameobject
 
-    public int difficulty = 2;  // Game difficulty 1-3 (easy-hard). Controls how the enemies will spawn.
+  [Range(1, 3)]
+  public int difficulty = 2;  // Game difficulty 1-3 (easy-hard). Controls how the enemies will spawn.
 
-    public float spawnInterval = 10f;   // Time between enemies spawning. Increases every "wave".
-    private float timePassed;           // Keeps track of the time that has passed. Resets every enemy spawn.
-    private bool isPaused = false;      // Pauses the spawner.
-    private float timeRemaining = 0;    // Keeps track of how much time is left of the pause.=
+  public float spawnInterval = 10f;   // Time between enemies spawning. Increases every "wave".
+  private float timePassed;           // Keeps track of the time that has passed. Resets every enemy spawn.
+  private bool isPaused = false;      // Pauses the spawner.
+  private float timeRemaining = 0;    // Keeps track of how much time is left of the pause.=
 
-    private void Update()
+  private void Update()
+  {
+    // Prevent the spawner form functioning when it is paused.
+    if (isPaused)
     {
-        // Prevent the spawner form functioning when it is paused.
-        if (isPaused) 
-        {
-            timeRemaining -= Time.deltaTime;    // Count down until pause has passed.
-            if (timeRemaining <= 0)
-            {
-                isPaused = false;   // Unpause the game when countdown has been reached.
-            }
+      timeRemaining -= Time.deltaTime;    // Count down until pause has passed.
+      if (timeRemaining <= 0)
+      {
+        isPaused = false;   // Unpause the game when countdown has been reached.
+      }
 
-            return;
-        }
-
-        timePassed += Time.deltaTime;   // Updates the counter until spawn
-        if(timePassed > spawnInterval) 
-        {
-            Instantiate(enemy); // Spawns enemy
-            timePassed = 0f;    // Resets counter
-        }
+      return;
     }
 
-    /* #region Description */
-    // <summary>
-    //   Gets called when a new wave has begun. Sets conditions for the new wave.
-    // </summary>
-    /* #endregion */
-    public void newWave()
+    timePassed += Time.deltaTime;   // Updates the counter until spawn
+    if (timePassed > spawnInterval)
     {
-        isPaused = true;
-        spawnInterval *= 0.8f;
+      // Generate space in front of player (now back though??) based on the selected difficulty
+      Vector3 enemySpawn = transform.position +
+        player.transform.forward * (5 / (float)difficulty) +
+        player.transform.right * (Random.Range(-5, 5) / (float)difficulty);
+      enemySpawn.y = 1f;                                    // Make sure enemy spawns above ground
+      Instantiate(enemy, enemySpawn, transform.rotation);   // Spawns enemy
+      timePassed = 0f;                                      // Resets counter
     }
+  }
 
-    /* #region Description */
-    // <summary>
-    //   Pauses the spawner for a set amount of time
-    // </summary>
-    /* #endregion */
-    public void pauseSpawner(float time)
-    {
-        isPaused = false;
-        timeRemaining = time;
-    }
+  /* #region Description */
+  // <summary>
+  //   Gets called when a new wave has begun. Sets conditions for the new wave.
+  // </summary>
+  /* #endregion */
+  public void newWave()
+  {
+    isPaused = true;
+    spawnInterval *= 0.8f;
+  }
+
+  /* #region Description */
+  // <summary>
+  //   Pauses the spawner for a set amount of time
+  // </summary>
+  /* #endregion */
+  public void pauseSpawner(float time)
+  {
+    isPaused = false;
+    timeRemaining = time;
+  }
 }

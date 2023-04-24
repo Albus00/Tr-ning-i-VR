a5ac402ab22be4649db3a0a2eb2ac6e2c29f12e6
ivaga541 Gavelin <ivargavelin@gmail.com>
2023-04-24 11:14:31 +0200

    Improved rewarding system
    Added Timer + nicer kill UI + Death sound effect

--- Assets/Scripts/Improvements/EnemyBehaviour.cs
+++ Assets/Scripts/Improvements/EnemyBehaviour.cs
@@ -10,7 +10,11 @@ using UnityEngine;
 //Set up mixamo model: https://www.youtube.com/watch?v=KuMe6Iz8pFI
 public class EnemyBehaviour : MonoBehaviour
 {
+    // Counter for kills and variables for playing death sound
     KillCounter killCounterScript;
+    public AudioSource source;
+    public AudioClip clip;
+
     // OBS: State machine används inte än.
     // --------- Enemy State --------- //
     private enum EnemyState
@@ -86,7 +90,12 @@ public class EnemyBehaviour : MonoBehaviour
 
     private void EnableRagdoll() // when die, go ragdoll
     {
+        // Add kill to killcounter
         killCounterScript.AddKill();
+
+        // Play death sound
+        source.PlayOneShot(clip);
+
         foreach (var rigidbody in ragdollRigidbodies)
         {
             rigidbody.isKinematic = false; // enables physics  affecting the rigidbodies in the child objects like arms, legs etc

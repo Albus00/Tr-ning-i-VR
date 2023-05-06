5cd573a780cb6b936f1d7a63934df99746e2d615
Klas Nordqvist <klano953@student.liu.se>
2023-05-06 11:18:10 +0200

    Revert "merging"
    This reverts commit 4db38651954bb305c3dfe85d293ebd84918968f1, reversing
    changes made to d5e6e6c0eae412a55d1704f856e55874652ac61c.

--- Assets/Scripts/DifficultyManager.cs
+++ Assets/Scripts/DifficultyManager.cs
@@ -7,13 +7,28 @@ using TMPro;
 public class DifficultyManager : MonoBehaviour
 {
     [SerializeField] private Slider difficultySlider;
+    [SerializeField] private TextMeshProUGUI difficultyVal;
     [SerializeField] private Slider gameificationSlider;
+    [SerializeField] private TextMeshProUGUI gameificationVal;
 
-    public int difficulty = 1;
+    public int difficulty = 0;
     public int gameification = 3;
 
     void Start(){
         gameification = 3;
+    }
+
+    //Calling these functions directly from a slider seems to have broke in unity 2018.2
+    public void SetDifficulty(int newVal){
+        Debug.Log("Changed difficulty to " + newVal);
+        difficultyVal.text = newVal.ToString();
+        difficulty = newVal;
+    }
+
+    public void SetGameification(int newVal){
+        Debug.Log("Changed Gameification to " + newVal);
+        gameificationVal.text = newVal.ToString();
+        gameification = newVal;
     }
 
     void Awake(){
